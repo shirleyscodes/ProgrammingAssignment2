3d69f0609ae4ad697a62107d71f8704b2bfb6370
shirleyscodes <104778531+shirleyscodes@users.noreply.github.com>
2023-04-15 10:40:28 +0530

    added solutions for week 3🎉

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Mean of a Vector [Shirley Asangi]
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<-y
+    inv <<-NULL
+  }
+  get <- function()x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get = get,
+       setinverse = setinverse,
+       getinverse=getinverse
+       )
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinverse <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix_to_inverse <- x$get()
+  inv <- solve(matrix_to_inverse, ...)
+  x$setinverse(inv)
+  inv
+      ## Return a matrix that is the inverse of 'x'
 }
